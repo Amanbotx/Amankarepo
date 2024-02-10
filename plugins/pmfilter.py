0dad4faa0ea3c8982052aa1f146ff047d77133db
Amanvishwakarmabot <147085734+Amanvishwakarmabot@users.noreply.github.com>
2024-02-10 21:20:49 +0530

    Update pmfilter.py hi

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1544,7 +1544,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "other":
         buttons = [[
-            InlineKeyboardButton('☎️ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ', user_id=int(5977931010))
+            InlineKeyboardButton('☎️ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ', user_id=int(6883904115))
         ],[
             InlineKeyboardButton('⋞ ʙᴀᴄᴋ', callback_data='diamond'),
             InlineKeyboardButton('7 / 7', callback_data='pagesn1'),
