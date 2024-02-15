e25af1544924c1bee9026afcacc3eef8de6d159b
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-02-15 20:26:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,7 +79,7 @@ async def start(client, message):
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, message.from_user.id, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME, message.from_user.id,),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -150,7 +150,7 @@ async def start(client, message):
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, message.from_user.id, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME, message.from_user.id,),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
