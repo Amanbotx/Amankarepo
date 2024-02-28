fb4969db7429bbe78a2cb0f48d3a4a88ef8c885b
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-02-28 18:41:41 +0530

    Update grouprule.py

--- plugins/Extra/grouprule.py
+++ plugins/Extra/grouprule.py
@@ -10,7 +10,7 @@ async def plan(client,message):
     # Replace the following placeholders with your actual values
     button_text = '𝑩𝑼𝒀 𝑷𝑳𝑨𝑵'
     photo_url = 'https://graph.org/file/f8c26a2bda2c9ca9c6871.jpg'
-    plan_text = 'hi'  # Add your plan details here
+    plan_text = '♨️ 𝗚𝗥𝗢𝗨𝗣 𝗥𝗨𝗟𝗘𝗦 ♨️\n\n🔹 Sᴇᴀʀᴄʜ Mᴏᴠɪᴇ Wɪᴛʜ Cᴏʀʀᴇᴄᴛ Sᴘᴇʟʟɪɴɢ :\n› ᴀᴠᴀᴛᴀʀ 2009 ✅\n› ᴀᴠᴀᴛᴀʀ ʜɪɴᴅɪ ✅\n› ᴀᴠᴀᴛᴀʀ ᴍᴏᴠɪᴇ ❌\n› ᴀᴠᴀᴛᴀʀ ʜɪɴᴅɪ\nᴅᴜʙʙᴇᴅ..❌\n\n🔹Sᴇᴀʀᴄʜ Wᴇʙ Sᴇʀɪᴇs Iɴ ᴛʜɪs Fᴏʀᴍᴀᴛᴇ :\n› ᴠɪᴋɪɴɢs S01 ✅\n› ᴠɪᴋɪɴɢs S01E01 ✅\n› ᴠɪᴋɪɴɢs S01 ʜɪɴᴅɪ ✅\n› ᴠɪᴋɪɴɢs S01 ʜɪɴᴅɪ ᴅᴜʙʙ. ❌\n› ᴠɪᴋɪɴɢs sᴇᴀsᴏɴ 1 ❌\n› ᴠɪᴋɪɴɢs ᴡᴇʙ sᴇʀɪᴇs❌\n🔹 ᴅᴏɴ'ᴛ ᴅᴏ ᴀɴʏ sᴇʟғ ᴘʀᴏᴍᴏᴛɪᴏɴ.\n\n🔹 ᴅᴏɴ'ᴛ sᴇɴᴅ ᴀɴʏ ᴋɪɴᴅ ᴏғ ᴘʜᴏᴛᴏ, ᴠɪᴅᴇᴏ ᴅᴏᴄᴜᴍᴇɴᴛs, ᴜʀʟ ᴇᴛᴄ..\n\n🔹 ᴅᴏɴ'ᴛ ʀᴇǫᴜᴇsᴛ ᴀɴʏ ᴛʜɪɴɢs ᴏᴛʜᴇʀ ᴛʜᴀɴ ᴍᴏᴠɪᴇ sᴇʀɪᴇs ᴀɴɪᴍᴇs..\n\n⚙️ 𝖭ᴏᴛᴇ :- 𝖠ʟʟ ᴍᴇ𝗌𝗌ᴀɢᴇ𝗌 ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏ-ᴅᴇʟᴇᴛᴇᴅ ᴀғᴛᴇʀ 𝟷𝟶 ᴍɪɴᴜᴛᴇ𝗌 ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ ɪ𝗌𝗌ᴜᴇ𝗌.'  
 
     # Create an inline keyboard with the buy plan button
     keyboard = [[InlineKeyboardButton(button_text, url='https://t.me/none_090')
