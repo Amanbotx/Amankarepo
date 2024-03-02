6f2fb35efc6af48f5b8b9b1e2c2bbd60786cab31
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-02 22:12:27 +0530

    Update whois.py

--- plugins/Extra/whois.py
+++ plugins/Extra/whois.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pyrogram import Client, filters, enums
 from pyrogram.errors import UserNotParticipant
 from info import COMMAND_HAND_LER
-from plugins.helper..extract_user import extract_user
+from plugins.helper.extract_user import extract_user
 from plugins.helper.cust_p_filters import f_onw_fliter
 from plugins.helper.last_online_hlpr import last_online
 
