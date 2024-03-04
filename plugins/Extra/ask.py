abb080ad41132cc473f21827efb5f9bc5d66ba94
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-04 19:36:49 +0530

    Update ask.py

--- plugins/Extra/ask.py
+++ plugins/Extra/ask.py
@@ -4,9 +4,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import requests
 from info import LOG_CHANNEL, GOOGLE_API_KEY
-import google.generativeai as genai
 
-genai.configure(api_key=GOOGLE_API_KEY)
+
+
 # how to get the api key == https://t.me/sd_bots/256 (copy this link and search on telegram)
 
 @Client.on_message(filters.command("ai") & filters.group)
