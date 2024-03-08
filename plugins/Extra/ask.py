6cf64595f1400979b66ab6b317f599fd2b2f77b6
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-08 14:57:02 +0530

    Update ask.py

--- plugins/Extra/ask.py
+++ plugins/Extra/ask.py
@@ -9,69 +9,68 @@ import google.generativeai as genai
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-@Client.on_message(filters.command("ask") & filters.chat(SUPPORT_CHAT_ID)) # support work only added 
+@Client.on_message(filters.command("ai") & filters.group)
 async def ai_generate(client, message):
-   user_input = message.text.split()[1:]
+    user_input = message.text.split()[1:]
 
-   if not user_input:
-       await message.reply_text("command incomplete provide /ask hello /opeai hi")
-       return
+    if not user_input:
+        await message.reply_text("Please provide your question after /ai")
+        return
 
-   user_input = " ".join(user_input)
-    s = await message.reply_sticker("CAACAgUAAxkBAAKRsmXpvGmyAxjdTEhIOQEjg0yvn9FzAAIBAAPBJDExTOWVairA1m8eBA")
-  
+    user_input = " ".join(user_input)
+    s = await message.reply_sticker("CAACAgUAAxkBAAIj-mWlAjaflbkifrOJPnnxp2edkuD-AALPDAACzIApVcg9eEkNQbBGHgQ")
+
     if user_input.lower() in ["who is your owner", "what is your owner name"]:  
         buttons = [[
-            InlineKeyboardButton("developer", url="https://t.me/RUhviiX1txdiOWFl")
+            InlineKeyboardButton("developer", url="https://t.me/BOT_OWNER26")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_sticker("CAACAgUAAxkBAAKRsmXpvGmyAxjdTEhIOQEjg0yvn9FzAAIBAAPBJDExTOWVairA1m8eBA")
+        await message.reply_sticker("CAACAgUAAxkBAAIjWGWkDiJW1Dyn6n8CjbbwxExf0FEIAAJyCgACywLBVKKgVw2dk9PbHgQ")
         await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}", reply_markup=reply_markup)
         return
 
-   generation_config = {
-       "temperature": 0.9,
-       "top_p": 1,
-       "top_k": 1,
-       "max_output_tokens": 2048,
-   }
+    generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+    }
 
-   safety_settings = [
-       {
-           "category": "HARM_CATEGORY_HARASSMENT",
-           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-       },
-       {
-           "category": "HARM_CATEGORY_HATE_SPEECH",
-           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-       },
-       {
-           "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-       },
-       {
-           "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-           "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-       },
-   ]
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
 
-   model = genai.GenerativeModel(
-       model_name="gemini-pro",
-       generation_config=generation_config,
-       safety_settings=safety_settings
-   )
+    model = genai.GenerativeModel(
+        model_name="gemini-pro",
+        generation_config=generation_config,
+        safety_settings=safety_settings
+    )
 
-   prompt_parts = [user_input]
-   response = model.generate_content(prompt_parts)
-   await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}\n ǫᴜᴇʀʏ ɪs:- {user_input}\n\nResults:\n\n{response.text}")         
-   await client.send_message(LOG_CHANNEL, text=f"#ask ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
-   await s.delete()
-
-@Client.on_message(filters.command("ask"))
+    prompt_parts = [user_input]
+    response = model.generate_content(prompt_parts)
+    await message.reply_text(response.text)
+    await client.send_message(LOG_CHANNEL, text=f"#google_ai ʀᴇǫᴜᴇsᴛ ғʀᴏᴍ {message.from_user.mention}\nǫᴜᴇʀʏ ɪs:- {user_input}")
+    await s.delete()
+@Client.on_message(filters.command("ai") & filters.private)
 async def ai_generate_private(client, message):
   buttons = [[
-    InlineKeyboardButton("SᑌᑭᑭOᖇT GᖇOᑌᑭ ", url="https://t.me/RUhviiX1txdiOWFl")
+    InlineKeyboardButton("ɢʀᴏᴜᴘ", url="https://t.me/Amanchatgroup1")
   ]]
   reply_markup = InlineKeyboardMarkup(buttons)
-  await message.reply_sticker("CAACAgUAAxkBAAKRsmXpvGmyAxjdTEhIOQEjg0yvn9FzAAIBAAPBJDExTOWVairA1m8eBA")
-  await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}\nᴜsᴇ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ɪn SᑌᑭᑭOᖇT GᖇOᑌᑭ  👇 ", reply_markup=reply_markup)
+  await message.reply_sticker("CAACAgUAAxkBAAIjWGWkDiJW1Dyn6n8CjbbwxExf0FEIAAJyCgACywLBVKKgVw2dk9PbHgQ")
+  await message.reply_text(text=f"ʜᴇʏ {message.from_user.mention}\nᴜsᴇ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ɪɴ ɢʀᴏᴜᴘ", reply_markup=reply_markup)
