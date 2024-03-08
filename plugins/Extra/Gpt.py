32c3618e2bf39e4f177cb7ad445478515be2ebfd
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-08 14:30:38 +0530

    Update Gpt.py

--- plugins/Extra/Gpt.py
+++ plugins/Extra/Gpt.py
@@ -18,7 +18,8 @@ async def ai_generate(client, message):
        return
 
    user_input = " ".join(user_input)
-       await message.reply_sticker("CAACAgUAAxkBAAKRsmXpvGmyAxjdTEhIOQEjg0yvn9FzAAIBAAPBJDExTOWVairA1m8eBA")
+
+   
   
     if user_input.lower() in ["who is your owner", "what is your owner name"]:  
         buttons = [[
