6c7b97b2c9dbe83271b149af74389bb7abaf0a76
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-10 09:19:42 +0530

    Update refer.py

--- plugins/Extra/refer.py
+++ plugins/Extra/refer.py
@@ -5,9 +5,8 @@ async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
        		[ [ InlineKeyboardButton("𝑺𝑯𝑨𝑹𝑬 𝒀𝑶𝑼𝑹 𝑳𝑰𝑵𝑲" ,url=f"https://t.me/share/url?url=https://t.me/movie_hub09_bot?start={message.from_user.id}")
               ],[
-                InlineKeyboardButton('⋞ ʙᴀᴄᴋ', callback_data='free'),
-                InlineKeyboardButton('2 / 7', callback_data='pagesn1'),
-                InlineKeyboardButton('ɴᴇxᴛ ⋟', callback_data='silver')
+                InlineKeyboardButton('00', callback_data='00'),
+                InlineKeyboardButton('00', callback_data='00')
               ]   ])
     
     await message.reply_text(f"ʜᴇʏ {message.from_user.mention}, \nHᴇʀᴇ ɪꜱ ʏᴏᴜʀ ʀᴇғғᴇʀᴀʟ ʟɪɴᴋ:\nhttps://t.me/movie_090_bot?start={message.from_user.id}\nShare this link with your friends, Each time they join, You will get 10 refferal points and after 100 points you will get 1 month Premium subscription.\n\nइस लिंक को दोस्तों के साथ Share करें, जब वे शामिल होंगे, तो आपको 10 रेफरल पॉइंट मिलेंगे ,और 100 पॉइंट के बाद आपको 1 महीने की प्रीमियम सदस्यता Automatically मिल जायगी। ",reply_to_message_id = message.id,reply_markup=reply_markup,)
