91428b8eb0060bfda9e73bee493add4fdabe3c47
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-10 10:24:07 +0530

    Update refer.py

--- plugins/Extra/refer.py
+++ plugins/Extra/refer.py
@@ -3,12 +3,12 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 @Client.on_message(filters.private & filters.command(["refer"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("𝑺𝑯𝑨𝑹𝑬 𝒀𝑶𝑼𝑹 𝑳𝑰𝑵𝑲" ,url=f"https://t.me/share/url?url=https://t.me/movie_hub09_bot?start={message.from_user.id}")
+       		[ [ InlineKeyboardButton("𝑺𝑯𝑨𝑹𝑬 𝒀𝑶𝑼𝑹 𝑳𝑰𝑵𝑲" ,url=f"https://telegram.me/share/url?url=https://t.me/Testing_00100_bot?start=reff_{message.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83")
               ],[
                 InlineKeyboardButton('00', callback_data='00'),
                 InlineKeyboardButton('00', callback_data='00')
               ]   ])
     
-    await message.reply_text(f"ʜᴇʏ {message.from_user.mention}, \nHᴇʀᴇ ɪꜱ ʏᴏᴜʀ ʀᴇғғᴇʀᴀʟ ʟɪɴᴋ:\nhttps://t.me/movie_090_bot?start={message.from_user.id}\nShare this link with your friends, Each time they join, You will get 10 refferal points and after 100 points you will get 1 month Premium subscription.\n\nइस लिंक को दोस्तों के साथ Share करें, जब वे शामिल होंगे, तो आपको 10 रेफरल पॉइंट मिलेंगे ,और 100 पॉइंट के बाद आपको 1 महीने की प्रीमियम सदस्यता Automatically मिल जायगी। ",reply_to_message_id = message.id,reply_markup=reply_markup,)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention}, \n\nHᴇʀᴇ ɪꜱ ʏᴏᴜʀ ʀᴇғғᴇʀᴀʟ ʟɪɴᴋ:\nhttps://t.me/Testing_00100_bot?start=reff_{message.from_user.id}\n\nShare this link with your friends, Each time they join, You will get 10 refferal points and after 100 points you will get 1 month Premium subscription.\n\nइस लिंक को दोस्तों के साथ Share करें, जब वे शामिल होंगे, तो आपको 10 रेफरल पॉइंट मिलेंगे ,और 100 पॉइंट के बाद आपको 1 महीने की प्रीमियम सदस्यता Automatically मिल जायगी। ",reply_to_message_id = message.id,reply_markup=reply_markup,)
     
 
