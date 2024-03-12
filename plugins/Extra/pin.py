484f835f825d753d50d12add7643bd095ce3b4cc
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-12 19:59:07 +0530

    Update pin.py

--- plugins/Extra/pin.py
+++ plugins/Extra/pin.py
@@ -3,14 +3,14 @@ from pyrogram.types import Message
 from plugins.helper.admin_check import admin_fliter                         
 
 @Client.on_message(filters.command("pin") & admin_fliter)
-async def pin(_, message: Message):
+async def pin(bot, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
 
 @Client.on_message(filters.command("unpin") & admin_fliter)             
-async def unpin(_, message: Message):
+async def unpin(bot, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.unpin()
