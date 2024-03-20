f94eae6333341d9955461a173d5b630b7c7db0a8
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-20 12:03:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -340,16 +340,16 @@ async def start(client, message):
             )
         )
         await query.message.edit_text(
-                text="⋘ Pʟᴇᴀsᴇ Wᴀɪᴛ ⋙"
-            )
-            await asyncio.sleep(0.5)
-            await query.message.edit_text(
-                text="⋘ Lᴏᴀᴅɪɴɢ Dᴀᴛᴀ ⋙"
-            )
-            await asyncio.sleep(0.4)
-            await query.message.edit_text(
-                text="⋘ Cᴏᴍᴘʟᴇᴛᴇ! ⋙"
-            )
+            text="⋘ Pʟᴇᴀsᴇ Wᴀɪᴛ ⋙"
+        )
+        await asyncio.sleep(0.5)
+        await query.message.edit_text(
+            text="⋘ Lᴏᴀᴅɪɴɢ Dᴀᴛᴀ ⋙"
+        )
+        await asyncio.sleep(0.4)
+        await query.message.edit_text(
+            text="⋘ Cᴏᴍᴘʟᴇᴛᴇ! ⋙"
+        )
         await asyncio.sleep(300)
         await k.edit("<b>ʏᴏᴜʀ ᴍᴇꜱꜱᴀɢᴇ ɪꜱ ᴅᴇʟᴇᴛᴇᴅ !\nᴋɪɴᴅʟʏ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ.</b>")
         return
@@ -386,16 +386,16 @@ async def start(client, message):
             )
         )
         await query.message.edit_text(
-                text="⋘ Pʟᴇᴀsᴇ Wᴀɪᴛ ⋙"
-            )
-            await asyncio.sleep(0.5)
-            await query.message.edit_text(
-                text="⋘ Lᴏᴀᴅɪɴɢ Dᴀᴛᴀ ⋙"
-            )
-            await asyncio.sleep(0.4)
-            await query.message.edit_text(
-                text="⋘ Cᴏᴍᴘʟᴇᴛᴇ! ⋙"
-            )
+            text="⋘ Pʟᴇᴀsᴇ Wᴀɪᴛ ⋙"
+        )
+        await asyncio.sleep(0.5)
+        await query.message.edit_text(
+            text="⋘ Lᴏᴀᴅɪɴɢ Dᴀᴛᴀ ⋙"
+        )
+        await asyncio.sleep(0.4)
+        await query.message.edit_text(
+            text="⋘ Cᴏᴍᴘʟᴇᴛᴇ! ⋙"
+        )
         await asyncio.sleep(600)
         await k.edit("<b>ʏᴏᴜʀ ᴍᴇꜱꜱᴀɢᴇ ɪꜱ ᴅᴇʟᴇᴛᴇᴅ !\nᴋɪɴᴅʟʏ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ.</b>")
         return
