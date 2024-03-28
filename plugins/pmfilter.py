212c29572fa5363162788d66fd0b09693caf3613
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-28 07:43:18 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1907,7 +1907,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
 
-            )
+           )
     elif query.data == "help":
         buttons = [[
             InlineKeyboardButton('• ʙᴏᴛ ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅꜱ •', callback_data='admic')
